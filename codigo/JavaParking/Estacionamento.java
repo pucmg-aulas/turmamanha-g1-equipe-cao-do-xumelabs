08c8c0db1c332744aed9d433a8b2e3e9795aca3f
Paulo Assis <passis@sga.pucminas.br>
2024-09-22 20:33:07 -0300

    Update Estacionamento.java

--- codigo/JavaParking/Estacionamento.java
+++ codigo/JavaParking/Estacionamento.java
@@ -4,19 +4,37 @@ public class Estacionamento {
 private Vaga vaga[];
 private int numeroDeVagas;
 private UsoDeVaga usoDeVaga;
-private Cliente cliente[];
 private double valorCobranca;
+private static int vagasCadastradas;
 
-private void calcularCobranca() {
+static {
+	vagasCadastradas = 0;
+}
+
+public Estacionamento(int numeroDeVagas) {
+	this.numeroDeVagas = numeroDeVagas;
+	this.vaga = new Vaga[this.numeroDeVagas];
+}
+
+private double calcularCobranca() {
 	this.valorCobranca = (this.usoDeVaga.getTempo()/15) * 4;
 	if(this.valorCobranca >= 50) {
 		this.valorCobranca = 50.0;
 	}
-}
-
-public double getValorCobranca() {
 	return this.valorCobranca;
 }
 
+public double getValorCobranca() {
+	return calcularCobranca();
+}
+
+public boolean cadastrarVaga(Vaga vaga) {
+	if(vagasCadastradas < this.numeroDeVagas) {
+		this.vaga[vagasCadastradas] = vaga;
+		vagasCadastradas++;
+		return true;
+	}
+	return false;
+}
 
 }
