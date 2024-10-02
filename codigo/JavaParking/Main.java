fc7fb129b996c8c9e267717f2bdf947b5dade2dd
Paulo Assis <passis@sga.pucminas.br>
2024-10-02 08:57:33 -0300

    criando o menu

--- codigo/JavaParking/Main.java
+++ codigo/JavaParking/Main.java
@@ -2,21 +2,111 @@ package JavaParking;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main {	
 
-	public String quantidadeDeVagas() {
-		return "=============================================\n" +
-			   "  Bem-vindo ao JavaParking System!\n" +
-			   "=============================================\n\n" +
-			   "Por favor, informe o número de vagas disponíveis\n" +
-			   "para o seu estacionamento: ";
+    public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		Estacionamento e1 = new Estacionamento(10);
+		int opcao= 0 ;
+		
+		do{
+			System.out.println(Menu());
+			opcao = teclado.nextInt();
+
+			switch (opcao) {
+				case 1:
+					cadastrarVaga(teclado, e1);
+					break;
+			
+				case 2:
+					
+					break;
+
+				case 3:
+					
+					break;
+
+				case 4:
+					
+					break;
+
+				case 5:
+					
+					break;
+				default:
+					break;
+			}
+		}while(opcao != 5);
+    }
+
+	// Método para exibir o menu
+    private static String Menu() {
+		return """
+				===============================
+					JAVA PARKING SYSTEM
+				===============================
+				1. Cadastrar Vaga
+				2. Cadastrar Cliente
+				3. Cadastrar Veículo
+				4. Estacionar Carro
+				5. Desocupar Vaga
+				6. Sair
+				===============================
+				Digite a opção desejada: 
+				""";
 	}
 
-	public String numeroVaga(){
-		return "Digite o numero alfanumerico da Vaga: ";
+	private static void cadastrarVaga(Scanner teclado, Estacionamento estacionamento){
+		System.out.println("Digite o numero alfanumerico da Vaga: ");
+		String numeroVaga = teclado.nextLine();
+
+		System.out.println(MenuDeTipoDeVaga());
+		int tipoDeVaga = teclado.nextInt();
+
+		switch (tipoDeVaga) {
+			case 1:
+				VagaIdoso vagaIdoso = new VagaIdoso(numeroVaga);
+				if(estacionamento.cadastrarVaga(vagaIdoso)){
+					System.out.println("Cadastrada com sucesso\n");
+				}else{
+					System.out.println("Nome de Vaga ja existente\n");
+				}
+				break;
+
+			case 2:
+				VagaPcd vagaPcd = new VagaPcd(numeroVaga);
+				if(estacionamento.cadastrarVaga(vagaPcd)){
+					System.out.println("Cadastrada com sucesso\n");
+				}else{
+					System.out.println("Nome de Vaga ja existente\n");
+				}
+				break;
+
+			case 3:
+				VagaVip vagaVip = new VagaVip(numeroVaga);
+				if(estacionamento.cadastrarVaga(vagaVip)){
+					System.out.println("Cadastrada com sucesso\n");
+				}else{
+					System.out.println("Nome de Vaga ja existente\n");
+				}
+				break;
+
+			case 4:
+				VagaDefault vagaDefault = new VagaDefault(numeroVaga);
+				if(estacionamento.cadastrarVaga(vagaDefault)){
+					System.out.println("Cadastrada com sucesso\n");
+				}else{
+					System.out.println("Nome de Vaga ja existente\n");
+				}
+				break;
+		
+			default:
+				System.out.println("Opcao inválida");
+				break;
+		}
 	}
 
-	public String tipoDeVaga() {
+	private static String MenuDeTipoDeVaga() {
 		return "=============================================\n" +
 			   "  Selecione o Tipo de Vaga\n" +
 			   "=============================================\n\n" +
@@ -25,72 +115,5 @@ public class Main {
 			   "3. Vaga VIP\n" +
 			   "4. Vaga Normal\n\n" +
 			   "Digite a opção desejada: ";
-	}	
-
-    // Método para exibir o menu
-    public String Menu() {
-        // Melhorando o estilo da string do menu
-        return """
-                ===============================
-                       JAVA PARKING SYSTEM
-                ===============================
-                1. Cadastrar Vaga
-				2. Cadastrar Cliente
-                3. Cadastrar Veículo
-                4. Estacionar Carro
-				5. Desocupar Vaga
-                ===============================
-                Digite a opção desejada: 
-                """;
-    }
-
-    public static void main(String[] args) {
-		
-        Main main = new Main();
-        Scanner teclado = new Scanner(System.in);
-
-		System.out.println(main.quantidadeDeVagas());
-		int quantidadeDeVagas = teclado.nextInt();
-
-		Estacionamento E1 = new Estacionamento(quantidadeDeVagas);
-        System.out.println(main.Menu());
-
-        int i = teclado.nextInt();
-        teclado.close();
-
-		switch (i) {
-			case 1:
-				System.out.println(main.numeroVaga());
-				String numeroVaga = teclado.nextLine();
-				System.out.println(main.tipoDeVaga());
-				int tipoDeVaga = teclado.nextInt();
-				
-			case 2:
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			case 5:
-				break;
-			default:
-				break;
-		}
-
-    }
+	}
 }
-
-/*Veiculo v1 = new Veiculo("12321");
-		Veiculo v2 = new Veiculo("36989");
-		VagaIdoso v7 = new VagaIdoso("155");
-		Cliente c1 = new Cliente();
-		Cliente c2 = new Cliente("pedro");
-		c1.cadastrarVeiculo(v1);
-		c2.cadastrarVeiculo(v2);
-
-		UsoDeVaga v9 = new UsoDeVaga(v2, v7);
-
-		Estacionamento e1 = new Estacionamento(1);
-		e1.cadastrarVaga(v7);
-		e1.estacionar(v9);
-		 */
