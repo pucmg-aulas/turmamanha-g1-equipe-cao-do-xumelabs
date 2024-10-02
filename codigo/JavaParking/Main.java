9cb8ed680ca0095de75f4f3acf8ef07ff909da62
Paulo Assis <passis@sga.pucminas.br>
2024-10-02 08:14:28 -0300

    tentando organizar a main

--- codigo/JavaParking/Main.java
+++ codigo/JavaParking/Main.java
@@ -4,6 +4,29 @@ import java.util.Scanner;
 
 public class Main {
 
+	public String quantidadeDeVagas() {
+		return "=============================================\n" +
+			   "  Bem-vindo ao JavaParking System!\n" +
+			   "=============================================\n\n" +
+			   "Por favor, informe o número de vagas disponíveis\n" +
+			   "para o seu estacionamento: ";
+	}
+
+	public String numeroVaga(){
+		return "Digite o numero alfanumerico da Vaga: ";
+	}
+
+	public String tipoDeVaga() {
+		return "=============================================\n" +
+			   "  Selecione o Tipo de Vaga\n" +
+			   "=============================================\n\n" +
+			   "1. Vaga para Idoso\n" +
+			   "2. Vaga para PCD (Pessoa com Deficiência)\n" +
+			   "3. Vaga VIP\n" +
+			   "4. Vaga Normal\n\n" +
+			   "Digite a opção desejada: ";
+	}	
+
     // Método para exibir o menu
     public String Menu() {
         // Melhorando o estilo da string do menu
@@ -22,19 +45,42 @@ public class Main {
     }
 
     public static void main(String[] args) {
-		/* 
+		
         Main main = new Main();
         Scanner teclado = new Scanner(System.in);
 
+		System.out.println(main.quantidadeDeVagas());
+		int quantidadeDeVagas = teclado.nextInt();
+
+		Estacionamento E1 = new Estacionamento(quantidadeDeVagas);
         System.out.println(main.Menu());
 
         int i = teclado.nextInt();
         teclado.close();
 
-        System.out.println("Você escolheu a opção: " + i);
-		*/
+		switch (i) {
+			case 1:
+				System.out.println(main.numeroVaga());
+				String numeroVaga = teclado.nextLine();
+				System.out.println(main.tipoDeVaga());
+				int tipoDeVaga = teclado.nextInt();
+				
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			default:
+				break;
+		}
 
-		Veiculo v1 = new Veiculo("12321");
+    }
+}
+
+/*Veiculo v1 = new Veiculo("12321");
 		Veiculo v2 = new Veiculo("36989");
 		VagaIdoso v7 = new VagaIdoso("155");
 		Cliente c1 = new Cliente();
@@ -47,6 +93,4 @@ public class Main {
 		Estacionamento e1 = new Estacionamento(1);
 		e1.cadastrarVaga(v7);
 		e1.estacionar(v9);
-
-    }
-}
+		 */
