6c9fb033c6748848fb4a0e4e36410d3b5488892f
Paulo Assis <passis@sga.pucminas.br>
2024-10-25 10:43:18 -0300

    Update ClienteDAO.java

--- codigo/JavaParking/src/main/java/mvc/dao/ClienteDAO.java
+++ codigo/JavaParking/src/main/java/mvc/dao/ClienteDAO.java
@@ -14,7 +14,7 @@ public class ClienteDAO {
 
     private ClienteDAO() {
         this.clientes = new ArrayList<>();
-        carregarClientesTxt(); // Carrega os clientes do arquivo ao instanciar
+        carregarClientesTxt(); 
     }
 
     public static ClienteDAO getInstance() {
@@ -34,71 +34,25 @@ public class ClienteDAO {
     }
 
     public Cliente pesquisarClienteNome(String nome) {
-        String filePath = "clientes.txt";
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String linha;
-            while ((linha = reader.readLine()) != null) {
-                String[] partes = linha.split(";");
-                if (partes.length == 2) {
-                    String nomeCliente = partes[1].trim();
-                    if (nomeCliente.equalsIgnoreCase(nome)) {
-                        Cliente cliente = new Cliente(nomeCliente);
-                        cliente.setIdentificador(Integer.parseInt(partes[0].trim())); 
-                        return cliente; 
-                    }
-                }
+        for(Cliente cliente: clientes){
+            if(cliente.getNome().equals(nome)){
+                return cliente;
             }
-        } catch (IOException e) {
-            System.err.println("Erro ao pesquisar cliente: " + e.getMessage());
         }
         return null;
     }
     
 
     public Cliente pesquisarClienteId(int id) {
-        String filePath = "clientes.txt";
-    
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String linha;
-            while ((linha = reader.readLine()) != null) {
-                String[] partes = linha.split(";");
-                if (partes.length == 2) {
-                    int clienteId = Integer.parseInt(partes[0].trim());
-                    String nomeCliente = partes[1].trim();
-                    if (clienteId == id) {
-                        Cliente cliente = new Cliente(nomeCliente);
-                        cliente.setIdentificador(clienteId); 
-                        return cliente; 
-                    }
-                }
+        for(Cliente cliente: clientes){
+            if(cliente.getIdentificador() == id){
+                return cliente;
             }
-        } catch (IOException e) {
-            System.err.println("Erro ao pesquisar cliente: " + e.getMessage());
         }
-        return null; 
+        return null;
     }    
 
     public ArrayList<Cliente> listaDeClientes() {
-        ArrayList<Cliente> clientes = new ArrayList<>(); 
-    
-        String filePath = "clientes.txt";
-    
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String linha;
-            while ((linha = reader.readLine()) != null) {
-                String[] partes = linha.split(";");
-                if (partes.length == 2) {
-                    int clienteId = Integer.parseInt(partes[0].trim());
-                    String nomeCliente = partes[1].trim();
-                    Cliente cliente = new Cliente(nomeCliente);
-                    cliente.setIdentificador(clienteId); 
-                    clientes.add(cliente);
-                }
-            }
-        } catch (IOException e) {
-            System.err.println("Erro ao carregar lista de clientes: " + e.getMessage());
-        }
         return clientes; 
     }
     
